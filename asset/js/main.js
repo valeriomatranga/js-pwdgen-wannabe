415356a1fa252638b3eade64061df3805d6e45c9
valeriomatranga <matrangav@gmail.com>
2021-04-22 15:42:08 +0200

    generatore password

---
+++ asset/js/main.js
@@ -0,0 +1,12 @@
+//chiedi il nome
+var nome = prompt("quale e il tuo nome?");
+
+//chiedi il cognome
+var cognome = prompt("quale e il tuo cognome?");
+
+//chiedi il suo colore preferito
+var colore = prompt("quale e il tuo colore preferito?");
+
+//combinazione password
+var password = document.getElementById("password");
+password.innerHTML = "la tua password e: " + nome + cognome + colore + 21
